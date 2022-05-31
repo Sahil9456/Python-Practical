1b95e3cda7e132fdc46e718a7fba2bbc9e75a22b
Sahil9456 <106598806+Sahil9456@users.noreply.github.com>
2022-05-31 19:36:51 +0530

    Create range.py

---
+++ Miscellaneous/Random/range.py
@@ -0,0 +1,14 @@
+import random
+
+# Using randrange() to generate numbers from 0-100
+print ("Random number from 0-100 is : ",end="")
+print (random.randrange(100))
+
+# Using randrange() to generate numbers from 50-100
+print ("Random number from 50-100 is : ",end="")
+print (random.randrange(50,100))
+
+# Using randrange() to generate numbers from 50-100
+# skipping 5
+print ("Random number from 50-100 skip 5 is : ",end="")
+print (random.randrange(50,100,5))
